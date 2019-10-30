ebaab5b18714790615a313c3d416a7683993cfac
djtrueway <djtrueway@gmail.com>
2019-10-30 18:43:04 +0000

    final 2

--- wp-config.php
+++ wp-config.php
@@ -22,23 +22,8 @@
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'bitnami_wordpress' );
 
-/** setting for production 
- * MySQL database username
- * define( 'DB_USER', 'bn_wordpress' );
- * 
- * MySQL database password
- * define( 'DB_PASSWORD', 'b1c599ea7f' );
- * 
- * 
- * MySQL hostname
- * define( 'DB_HOST', 'localhost:3306' );
-*/
-
-/** MySQL database username */
-define( 'DB_USER', 'root' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+  define( 'DB_USER', 'bn_wordpress' );
+  define( 'DB_PASSWORD', 'b1c599ea7f' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost:3306' );
